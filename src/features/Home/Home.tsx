76ae3dfcd1285266a9ef9c335c769e8c3cecda27
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-04-27 21:23:29 -0700

    feat: implement Home component for CSV file analysis

---
+++ src/features/Home/Home.tsx
@@ -0,0 +1,23 @@
+import Navbar from "../../shared/components/Navbar";
+import UploadFile from "./components/UploadFile";
+
+const Home = () => {
+  return (
+    <div className="w-full  justify-center items-center flex flex-col">
+      <section className="pt-5 flex flex-col justify-center items-center w-full">
+        <header className="flex flex-col justify-center items-center my-3 space-y-5">
+          <h2 className="font font-sans text-4xl">CSV File Analyser</h2>
+          <p className=" text-center max-w-3xl text-gray-500 font-medium">
+            Upload your CSV files to analyze patterns and trends using AI. Our
+            system will process your data and provide insights
+          </p>
+        </header>
+        <div className="w-1/2 mt-5 flex flex-col justify-center items-center">
+          <UploadFile />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
