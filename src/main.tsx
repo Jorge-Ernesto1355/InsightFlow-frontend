93639cb210329d0eee4c3d6a8b444e93f88ee273
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-04-27 21:22:54 -0700

    feat: integrate Ant Design and React Router into main application structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ConfigProvider, App as AndtApp } from "antd";
+import { RouterProvider } from "react-router-dom";
+import "antd/dist/reset.css";
+import { router } from "./app/router/Routes";
+
+createRoot(document.getElementById("root")!).render(
+  <ConfigProvider>
+    <AndtApp>
+      <RouterProvider router={router} />
+    </AndtApp>
+  </ConfigProvider>
+);
