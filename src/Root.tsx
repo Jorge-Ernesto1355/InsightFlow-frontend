1b88174df107694f1b4c29cbadb688b91e4db647
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-04-27 21:23:06 -0700

    feat: create Root component with Navbar and Home integration

---
+++ src/Root.tsx
@@ -0,0 +1,13 @@
+import Home from "./features/Home/Home";
+import Navbar from "./shared/components/Navbar";
+
+function Root() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default Root;
