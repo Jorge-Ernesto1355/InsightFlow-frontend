392dc776f31f8b2d4453892ec5101d424ed3b888
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2025-04-30 20:20:05 -0700

    feat: add Layout component to structure pages with Navbar

---
+++ src/shared/components/Layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div><Navbar/>{children}</div>
+  )
+}
+
+export default Layout
