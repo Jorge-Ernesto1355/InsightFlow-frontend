65550d733fbf3a5b56585a8ac9f6b77e4ce7d212
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2025-04-30 20:20:19 -0700

    refactor: improve Navbar layout and styling for better readability and consistency

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -2,26 +2,26 @@ import { Database, StatsUpSquare, Upload } from "iconoir-react";
 
 const Navbar = () => {
   return (
-    <nav className="w-full px-9 p-3 border-b-2 border-gray-200 flex justify-between items-center">
+    <nav className="w-full px-6  border-b-2 border-gray-200 flex justify-between items-center">
       <div>
-        <h2 className="text-2xl font-bold">InsightFlow</h2>
-        <p className="text-sm text-gray-600 font-medium">CSV Analyzer</p>
+      <h2 style={{marginBottom: "0px"}} className="text-2xl font-bold  ">InsightFlow</h2>
+      <p className="text-xs text-gray-500 font-medium">CSV Analyzer</p>
       </div>
       <div>
-        <ul className="flex gap-4">
-          <li className="flex justify-center items-center cursor-pointer focus:border-2 focus:border-white focus:bg-black focus:text-white p-2 focus:p-2 rounded transition-all duration-200">
-            <Upload className="mr-1" />
-            <p className="text-md font-medium">Upload</p>
-          </li>
-          <li className="flex justify-center items-center cursor-pointer focus:border-2 focus:border-white focus:bg-black focus:text-white p-2 focus:p-2 rounded transition-all duration-200">
-            <Database />
-            <p className="text-md font-medium"> Data View</p>
-          </li>
-          <li className="flex justify-center items-center cursor-pointer focus:border-2 focus:border-white focus:bg-black focus:text-white p-2 focus:p-2 rounded transition-all duration-200">
-            <StatsUpSquare className="mr-1" />
-            <p className="text-md font-medium">AI Analysis</p>
-          </li>
-        </ul>
+      <ul style={{marginBottom: "0px"}} className="flex gap-6 items-center w-full h-full justify-end">
+        <li className="flex justigy-center mt-2 items-center cursor-pointer hover:bg-gray-100 p-2 rounded transition-all duration-200">
+        <Upload className="mb-1 text-lg" />
+        <p className="text-sm font-medium">Upload</p>
+        </li>
+        <li className="flex items-center mt-2 cursor-pointer hover:bg-gray-100 p-2 rounded transition-all duration-200">
+        <Database className="mb-1 text-lg" />
+        <p className="text-sm font-medium">Data View</p>
+        </li>
+        <li className="flex  items-center mt-2 cursor-pointer hover:bg-gray-100 p-2 rounded transition-all duration-200">
+        <StatsUpSquare className="mb-1 text-lg" />
+        <p className="text-sm font-medium">AI Analysis</p>
+        </li>
+      </ul>
       </div>
     </nav>
   );
