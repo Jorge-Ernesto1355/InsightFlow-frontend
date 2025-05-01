d8e655250720cb6787af4e3ebce99573d0e79cd9
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2025-04-30 20:19:58 -0700

    refactor: enhance RiskFactors component layout for improved structure and clarity

--- src/features/dashboard/pages/RiskFactors/RiskFactors.tsx
+++ src/features/dashboard/pages/RiskFactors/RiskFactors.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+
 
 const RiskFactors = () => {
-  return <div>RiskFactors</div>;
+  return <div className="w-full p-4 m-4 h-screen border-1 border-gray-300 rounded-md">
+    <h3>Risk Factors Distribution Group</h3>
+    <p>Distribution of the differents risk factors clusters based on the groups diferences</p>
+  </div>;
 };
 
 export default RiskFactors;
