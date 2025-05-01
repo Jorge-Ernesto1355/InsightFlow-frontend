3ed31c024640e61f8617786e00778738c2a3a743
Jorge Ernesto laptop <jorgais.ernesto@gmail.com>
2025-04-30 20:19:50 -0700

    refactor: update Overview component layout for improved structure and content clarity

--- src/features/dashboard/pages/overview/Overview.tsx
+++ src/features/dashboard/pages/overview/Overview.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Overview = () => {
-  return <div>Overview</div>;
+  return <div className="w-full p-4 m-4 h-screen border-1 border-gray-300 rounded-md">
+  <h3 className="font-bold text-3xl">Risk Distribution Group</h3>
+  <p>Distribution of the differents risk factors clusters based on the groups diferences</p>
+</div>;
 };
 
 export default Overview;
