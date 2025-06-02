8466cdfcaeb8d134c577ddb733cae95e93a83d09
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-06-02 16:08:34 -0700

    refactor: wrap RouterProvider with QueryClientProvider for improved data handling

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { ConfigProvider, App as AndtApp } from "antd";
 import { RouterProvider } from "react-router-dom";
 import "antd/dist/reset.css";
 import { router } from "./app/router/Routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./app/router/providers/QueryClient";
 
 createRoot(document.getElementById("root")!).render(
   <ConfigProvider>
     <AndtApp>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </AndtApp>
   </ConfigProvider>
 );
