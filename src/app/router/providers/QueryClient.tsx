6e2a3174f8fd0c3c5f39d389390ae79450f333f4
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-06-02 16:08:54 -0700

    feat: add QueryClient configuration for React Query

---
+++ src/app/router/providers/QueryClient.tsx
@@ -0,0 +1,13 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    },
+    mutations: {
+      retry: 3,
+    },
+  },
+});
