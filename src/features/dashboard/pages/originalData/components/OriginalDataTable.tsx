f9289de3094f8dbb8d17bf380b1aea4bf8e6c076
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-06-02 16:12:10 -0700

    feat: add OriginalDataTable component for visualizing original data with cluster assignment

---
+++ src/features/dashboard/pages/originalData/components/OriginalDataTable.tsx
@@ -0,0 +1,37 @@
+import { Card, Table } from "antd";
+
+import getColumnsOriginalData from "../utils/getColumnsOriginalData";
+import getDataSourceOriginalData from "../utils/getDataSourceOriginalData";
+
+interface Props {
+  data: any;
+}
+
+const OriginalDataTable = ({ data }: Props) => {
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const columns = getColumnsOriginalData(data);
+
+  const dataSource = getDataSourceOriginalData(data);
+
+  return (
+    <Card className="w-full h-full border mt-5 border-gray-200 rounded-lg shadow transition-shadow hover:shadow-lg p-2">
+      <span className="text-2xl font-bold font-inter">Original Data</span>
+      <p className="text-gray-600 font-medium text-md mt-2 mb-4 font-inter">
+        Visualization of the multiples data with asignation of clusters
+      </p>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{ pageSize: 15 }}
+        bordered={false}
+        className="custom-ant-table"
+        scroll={{ x: true }}
+      />
+    </Card>
+  );
+};
+
+export default OriginalDataTable;
