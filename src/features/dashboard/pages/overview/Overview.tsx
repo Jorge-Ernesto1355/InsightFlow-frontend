56705f278ff536f59f22a9698b8aeb67a8f464de
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-06-02 16:13:09 -0700

    feat: refactor Overview component layout and structure for improved readability

--- src/features/dashboard/pages/overview/Overview.tsx
+++ src/features/dashboard/pages/overview/Overview.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { Col, Row } from "antd";
+import { mockData } from "../../../../../mockData";
+import ResumeCSVFile from "./ResumeCSVFile";
+import ResumeClusters from "./ResumeClusters";
+import CentroidsDistribution from "./CentroidsDistribution";
+import ImportanceVariable from "./ImportanceVariable";
+import { ExtractClustersSummaries } from "./utils/ExtractClustersSummaries";
 
 const Overview = () => {
-  return <div className="w-full p-4 m-4 h-screen border-1 border-gray-300 rounded-md">
-  <h3 className="font-bold text-3xl">Risk Distribution Group</h3>
-  <p>Distribution of the differents risk factors clusters based on the groups diferences</p>
-</div>;
+  return (
+    <div className=" p-4 border-1 font-inter border-gray-300 rounded-md">
+      <ResumeCSVFile
+        clusters={mockData.data.clusters}
+        metadata={{
+          ...mockData.data.metadata,
+          time: mockData.processing_time_ms,
+        }}
+      />
+      <Row gutter={[16, 16]} className="mt-5">
+        <Col span={10}>
+          <ResumeClusters
+            clusters={ExtractClustersSummaries(mockData.data.cluster_summaries)}
+          />
+        </Col>
+        <Col span={10}>
+          <CentroidsDistribution mockData={mockData} />
+        </Col>
+      </Row>
+      <ImportanceVariable
+        featureImportance={mockData.data.feature_importance}
+      />
+    </div>
+  );
 };
 
 export default Overview;
