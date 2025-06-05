629be307dab8e075aac34d3867951cbec837e3ea
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-06-04 22:41:07 -0700

    refactor: restructure Home component layout and improve content presentation

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,18 +1,33 @@
-import UploadFile from "./components/UploadFile";
+import { FileSpreadsheet } from "lucide-react";
+import Advantages from "./components/Advantages";
+
+import { advantages } from "./utils/Advantage";
+import Dragger from "./components/Dragger";
+import SelectFile from "./components/SelectFile";
+import { Divider } from "antd";
 
 const Home = () => {
   return (
     <div className="w-full  justify-center items-center flex flex-col">
-      <section className="pt-5 flex flex-col justify-center items-center w-full">
-        <header className="flex flex-col justify-center items-center my-3 space-y-5">
-          <h2 className="text-4xl  font-inter font-bold">CSV File Analyser</h2>
-          <p className=" text-center max-w-3xl text-gray-500 font-medium">
-            Upload your CSV files to analyze patterns and trends using AI. Our
-            system will process your data and provide insights
-          </p>
-        </header>
-        <div className="w-1/2 mt-5 flex flex-col justify-center items-center">
-          <UploadFile />
+      <header className=" mt-10 flex flex-col w-full justify-center items-center">
+        <div className="inline-flex items-center justify-center w-16 h-16 bg-blue-600 rounded-2xl mb-6">
+          <FileSpreadsheet className="h-8 w-8 text-white" />
+        </div>
+        <h2 className="text-5xl font-inter font-bold text-gray-900 mb-4">
+          CSV File Analyser
+        </h2>
+        <p className="font-inter text-xl text-gray-600 max-w-2xl mx-auto ">
+          {" "}
+          Upload and manage your CSV files with ease, Drag, drop, and process
+          your data files in seconds.
+        </p>
+      </header>
+      <section className="pt-5 flex flex-col  items-center m-auto w-full">
+        <Advantages advantages={advantages} />
+        <div className="w-4/5 mx-auto">
+          <Dragger />
+          <Divider className="my-5">Or</Divider>
+          <SelectFile />
         </div>
       </section>
     </div>
