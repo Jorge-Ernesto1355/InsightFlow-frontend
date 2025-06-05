e557069605ad7b5ead05d9002668e007ec47da77
Jorge Ernesto <jorgais.ernesto@gmail.com>
2025-06-05 15:11:55 -0700

    fix: refactor RiskFactors component to use data from store instead of mockData

--- src/features/dashboard/pages/RiskFactors/RiskFactors.tsx
+++ src/features/dashboard/pages/RiskFactors/RiskFactors.tsx
@@ -3,8 +3,11 @@ import PrincipalFactorsRisk from "./PrincipalFactorsRisk";
 import RiskPerCluster from "./RiskPerCluster";
 import { mockData } from "../../../../../mockData";
 import DetailsRiskFactors from "./components/DetailsRiskFactors";
+import useStore from "../../../../shared/store/AIStore";
 
 const RiskFactors = () => {
+  const data = useStore((data) => data.data);
+
   return (
     <div className="w-full p-4 m-4 h-screen border-1 border-gray-300 rounded-md">
       <span className="text-3xl font-bold font-inter">Risk Factors</span>
@@ -14,18 +17,16 @@ const RiskFactors = () => {
       </p>
       <Row gutter={[16, 16]} className="mt-5">
         <Col xs={24} md={12} lg={12}>
-          <PrincipalFactorsRisk
-            featureImportance={mockData.data.feature_importance}
-          />
+          <PrincipalFactorsRisk featureImportance={data.feature_importance} />
         </Col>
         <Col xs={24} md={12} lg={12}>
           <RiskPerCluster
-            clustersMap={mockData.data.clusters}
-            clusterSummaries={mockData.data.cluster_summaries}
+            clustersMap={data.clusters}
+            clusterSummaries={data.cluster_summaries}
           />
         </Col>
       </Row>
-      <DetailsRiskFactors />
+      <DetailsRiskFactors data={data || {}} />
     </div>
   );
 };
